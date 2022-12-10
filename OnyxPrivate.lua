1f4529081ac8fd7f9076d6223c4ccbffc0052f24
synape <105454056+synape@users.noreply.github.com>
2022-12-10 20:42:45 +0100

    Update OnyxPrivate.lua

--- OnyxPrivate.lua
+++ OnyxPrivate.lua
@@ -27,7 +27,7 @@ local MainTab = Window:CreateTab("Main", 4483362458)-- Title, Image
 
 local MainSection = MainTab:CreateSection("Texturepack")
 
-local RedBaseTp = MainTab:CreateButton({
+local Texturepack = MainTab:CreateButton({
 	Name = " MineCraft TexturePack",
 	Callback = function()
 		      local obj = game:GetObjects("rbxassetid://11144793662")[1]
@@ -47,6 +47,72 @@ local RedBaseTp = MainTab:CreateButton({
                     shared.con:Disconnect()
                 end)
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/milkwareclient/main/texture.lua"))()
+	end,
+})
+
+
+local Texturepack = MainTab:CreateButton({
+	Name = "Hot Texturepack",
+	Callback = function()
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["wood_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["wood_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["wood_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- stone
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["stone_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["stone_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["stone_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- iron
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["iron_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["iron_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["iron_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
+-- diamond
+game:GetObjects("rbxassetid://7062163349")[1].Parent = workspace
+
+workspace.Sword.Name = "Handle"
+workspace.Handle.Size = Vector3.new(0.03, 1.886, 1.385)
+local attach = game.ReplicatedStorage.Items["diamond_sword"]["Handle"]["RightGripAttachment"]:Clone()
+
+game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle:Destroy()
+
+workspace.Handle.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"]
+attach.Parent = game:GetService("ReplicatedStorage").Items["diamond_sword"].Handle
+attach.Position = Vector3.new(-4.37114e-08, 2.08716, 0.996195)
+attach.Axis = Vector3.new(-0.104528, -8.74228e-08, -0.994522)
+attach.Rotation = Vector3.new(180, -90, 0)
+attach.CFrame = CFrame.new(-4.37113989e-08, 2.08716011, 0.996195018, -4.37113883e-08, 0, -1, 8.74227766e-08, -1, -3.82137093e-15, -1, -8.74227766e-08, 4.37113883e-08)
 	end,
 })
 
