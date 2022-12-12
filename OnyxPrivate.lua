1415c80ca82fcfc082679f60e7d9d49422d4efae
synape <105454056+synape@users.noreply.github.com>
2022-12-12 12:12:16 +0100

    Update OnyxPrivate.lua

--- OnyxPrivate.lua
+++ OnyxPrivate.lua
@@ -150,109 +150,46 @@ local InfiniteYieldToggle = MainTab:CreateToggle({
 local TeleportTab = Window:CreateTab("Autowin stuff", 4483362458)
 local BedSection = TeleportTab:CreateSection("Teleports")
 
-local BlueBaseTp = TeleportTab:CreateButton({
-	Name = "DeathBedTp(gets every bed then stops)",
+local BedTp = TeleportTab:CreateButton({
+	Name = "BedTp",
 	Callback = function()
-repeat
-    task.wait()
-until game:IsLoaded()
-
-function xprint(s)
-if rconsoleprint then
-   rconsoleprint(s .. '\n')
-end
+		local mybed
+local pteam = game.Players.LocalPlayer.Team
+local myteam = game.Players.LocalPlayer.Team
+for i, v in pairs(game.Workspace:GetChildren()) do
+if v.Name == "bed" and v.Covers.BrickColor == myteam.TeamColor then
+mybed = v
 end
 
-function stop(s)
-    beds = false
-    for i, v in next, workspace:GetChildren() do
-        if v.Name == "bed" and tostring(v.Covers.BrickColor) == s then
-            x = true
-        end
-        if v.Name == 'bed' and tostring(v.Covers.BrickColor) ~= tostring(game:service'Players'.LocalPlayer.TeamColor) then
-            beds = true
-        end
-    end
-    if beds == false then
-        if getgenv().killed == nil then
-            game:service "Players".LocalPlayer.Character.Humanoid.Health = 0
-            getgenv().killed = true
-        end
-       return 1
-    end
-    if x == true then
-        x = false
-        return "hi"
-    else
-        return nil
-    end
 end
 
-function getPlr()
-    nMagnitude = nil
-    r = nil
-    for i,v in next, game:service'Players':GetPlayers() do
-          if v.Character and v.Character.PrimaryPart and v.TeamColor ~= game:service'Players'.LocalPlayer.TeamColor and v.Character:FindFirstChild('Humanoid') and v.Character.Humanoid.Health > 0 and v ~= game:service'Players'.LocalPlayer then
-                magnitude = (v.Character.PrimaryPart.Position - game:service'Players'.LocalPlayer.Character.PrimaryPart.Position).magnitude
-                if nMagnitude ~= nil then
-                    if magnitude < nMagnitude then
-                       r = v
-                       nMagnitude = magnitude
-                    end
-                else
-                    nMagnitude = magnitude
-                    r = v
-             end
-          end
-       end
-       return r
+
+
+local otherbed
+for i, v in pairs(game.Workspace:GetChildren()) do
+if v.Name == "bed" and v ~= mybed then
+otherbed = v
 end
 
-xprint('thank you sirmeme for posting this into robloxscripts ♥')
+end
+repeat wait(0.1) 
+for i, v in pairs(game.Workspace:GetChildren()) do
+if v.Name == "bed" and v ~= mybed then
+otherbed = v
+end
 
-function checkNetwork()
-local network_owner = getnetworkowner or isnetworkowner or networkowner or checknetworkowner or networkcheck
-if network_owner and network_owner(game:service'Players'.LocalPlayer.Character.PrimaryPart) == true then
+end
+if otherbed and otherbed.Name and otherbed.Transparency and otherbed.Parent == game.Workspace then
+ if game.Players.LocalPlayer.Character ~= nil and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = otherbed.CFrame + Vector3.new(0,5,0)
+end    
 else
-       if stop(tostring(game:service'Players'.LocalPlayer.TeamColor)) ~= nil then
-       workspace:WaitForChild(game:service'Players'.LocalPlayer.Name):WaitForChild('Humanoid').Health = 0
-       workspace:WaitForChild(game:service'Players'.LocalPlayer.Name):WaitForChild('Humanoid'):ChangeState('Dead')
-       workspace:WaitForChild(game:service'Players'.LocalPlayer.Name):WaitForChild('HumanoidRootPart').CFrame = CFrame.new(0,-500,0)
-       end
-    end
+otherbed = nil
+break;
 end
 
-game:GetService('StarterGui'):SetCore("SendNotification", {
-    Title = "Loaded!"; 
-    Text = "made by dotgg. modified a little bit by Ohio Final Boss"; 
-    Duration = 12; 
-})
 
-game:service "Players".LocalPlayer.Character.Humanoid.Health = 0
-
-local function bedtp()
-    task.wait(0.1)
-    char = game:service "Players".LocalPlayer.Character
-    bedtablex = {}
-    for i, v in next, workspace:GetChildren() do
-        if v.Name == "bed" then
-            table.insert(bedtablex, tostring(v.Covers.BrickColor))
-        end
-    end
-
-    for i,v in next, bedtablex do
-        for x,bed in next, workspace:GetChildren() do
-            if bed.Name == "bed" and tostring(bed.Covers.BrickColor) == v and tostring(bed.Covers.BrickColor) ~= tostring(game:service'Players'.LocalPlayer.TeamColor) then
-                xprint(v .. ' < bed target')
-                char:PivotTo(CFrame.new(bed.Position + Vector3.new(0,20,0)))
-                repeat task.wait() until stop(v) == nil
-                game:service "Players".LocalPlayer.Character.Humanoid.Health = 0
-            end
-        end
-    end
-end
-
-game:service "Players".LocalPlayer.CharacterAdded:Connect(bedtp)
+until otherbed == nil
 	end,
 })
 
